7a7dd242529e9fae477afe0d8494ea6f3783a902
徐旭 <xuxu_0816@163.com>
2019-05-22 13:00:44 +0800

    html表单提交评论

--- comment/models.py
+++ comment/models.py
@@ -13,3 +13,5 @@ class Comment(models.Model):
     comment_time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     
+    class Meta:
+        ordering = ['-comment_time']
